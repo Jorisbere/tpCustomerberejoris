1e3ef611c28ea99bf4694a0722d24680f48a5e0c
HP <HP@JORIS-BERE>
2026-06-04 10:14:03 +0000

    Ajout d'une page JSF

---
+++ src/main/java/com/mycustomer/tpcustomerberejoris/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycustomer.tpcustomerberejoris.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *Configuration JSF
+ * @author HP
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
